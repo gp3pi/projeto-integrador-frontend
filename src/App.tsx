34981f0e065dc93418d929409e2f61472b479112
Vitor Martins <vitormartinsxd@outlook.com>
2022-08-31 13:02:56 -0300

    Criando o rodapé da rede social.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './componentes/estaticos/footer/Footer';
 import './App.css';
+import Footer from './componentes/estaticos/footer/Footer';
 
 function App() {
   return (
-    <>
-    </>
+   <Footer/>
+    
   );
 }
 
