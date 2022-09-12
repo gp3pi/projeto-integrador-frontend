96a27d5cb39eb8f285c844a4ae625dc9cb3ad634
melissaJll <melissa.lequipe@gmail.com>
2022-09-12 08:39:50 -0300

    Acrescentei banner com o logo na pag Sobre Nos

--- src/paginas/sobrenos/SobreNos.tsx
+++ src/paginas/sobrenos/SobreNos.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Typography, Link } from '@material-ui/core';
 import { Box } from "@mui/material";
+import Banner from "../sobrenos/img/banner.png";
 import './SobreNos.css'
 
 function SobreNos(){
@@ -9,21 +10,24 @@ function SobreNos(){
 return (
     <>
       <Grid container>
-            <Grid item xs={12} className='imagemBanner'>
-                <Box padding={20}>
+            <Grid item xs={12} sm={12}>
+                <Box>
+                    <img src={Banner} alt="" className='imagemBanner'/>
                 </Box>
             </Grid>
+            
             <Grid container>
                 <Grid item xs={12} sm={2}/>
                 <Grid item xs={12} sm={8}>
                     <Box padding={5}>
                         <Typography className='fontMerri' variant='h4' gutterBottom color='textPrimary' component='h4' align='center'>Sobre Nós</Typography>
-                        <Typography  className='fontWork' align='center' variant='body1'>Este foi um projeto desenvolvido pelos alunos do bootcamp Generation Brasil com o intuito de colaborar com regiões 
+                        <Typography  className='fontWork' align='center' variant='body1'>Este foi um projeto desenvolvido pelos alunos do bootcamp Generation Brasil, em 2022, com o intuito de colaborar com regiões 
                             geograficamente vulneráveis, tais como as que carecem de recursos básicos ou que se encontram em situação bélica, 
                             e esse suporte, por parte dos usuários, poderá ocorrer através de doações e compartilhamento de informações
                         </Typography>
                         <Typography className='fontWork' variant='body1' align='center'>
-                        Todos os dias lemos notícias de situaçôes devastadoras como "Início da guerra na Ucrânia começou no dia 24 de fevereiro com a invasão do território do país pela Rússia. Taxa de assassinatos de indígenas aumenta 21,6% em dez anos enquanto de homicídios em geral cai. União Europeia dificulta regras de viagem para russos, mas rejeita proibição de visto. No continente africano, os albinos são estigmatizados, banidos e até mesmo assassinados. A Tanzânia, o Quénia e a África do Sul são países onde os albinos temem pelas suas vidas.
+                        A ideia para fundação da Telk foi inspirada pelos objetivos de desenvolvimento sustentável da ONU, mais espeficicamente pela ODS 16 (Paz, Justiça e Instituições Eficazes), que visa promover sociedades pacíficas e inclusivas para o desenvolvimento sustentável.
+                       
                         </Typography>
                     </Box>           
                 </Grid>
@@ -56,12 +60,14 @@ return (
                         <Link href="https://linktr.ee/ColaboradoresProjetoIntegrador">
                         <img className='perfis' src="https://i.pinimg.com/originals/54/bf/7a/54bf7a45856c608fe7165a908d57c7cf.png" alt="Foto de um integrante" />
                         </Link>
-                        <Typography  className='fontSans' align="center">Nome Completo</Typography>
+                        <Typography  className='fontSans' align="center">João Orlando</Typography>
                         <Typography  className='fontSans' align="center">Uma breve descrição deste membro</Typography>
                     </Box> 
                     <Box width='40%' className='padding'>
+                        <Link href="https://linktr.ee/ColaboradoresProjetoIntegrador">
                         <img className='perfis' src="https://i.pinimg.com/originals/54/bf/7a/54bf7a45856c608fe7165a908d57c7cf.png" alt="Foto de um integrante" />
-                        <Typography  className='fontSans' align="center">Nome Completo</Typography>
+                        </Link>
+                        <Typography  className='fontSans' align="center">Victor Hugo</Typography>
                         <Typography  className='fontSans' align="center">Uma breve descrição deste membro</Typography>
                     </Box>
                 </Grid>
@@ -69,23 +75,29 @@ return (
 
                 <Grid alignItems="center" item xs={6} sm={4}>
                     <Box padding={5}>
+                        <Link href="https://linktr.ee/ColaboradoresProjetoIntegrador">
                         <img className='perfis' src="https://i.pinimg.com/originals/54/bf/7a/54bf7a45856c608fe7165a908d57c7cf.png" alt="Foto de um integrante" />
-                        <Typography  className='fontSans' color="textPrimary" align="center">Nome Completo</Typography>
+                        </Link>
+                        <Typography  className='fontSans' color="textPrimary" align="center">Vitor Martins</Typography>
                         <Typography  className='fontSans' color="textPrimary" align="center">Github       Linkedin</Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={6} sm={4}>
                     <Box padding={5}>
+                        <Link href="https://linktr.ee/ColaboradoresProjetoIntegrador">
                         <img className='perfis' src="https://i.pinimg.com/originals/54/bf/7a/54bf7a45856c608fe7165a908d57c7cf.png" alt="Foto de um integrante" />
-                        <Typography  className='fontSans' color="textPrimary" align="center">Nome Completo</Typography>
+                        </Link>
+                        <Typography  className='fontSans' color="textPrimary" align="center">Hellen Sabo</Typography>
                         <Typography  className='fontSans' color="textPrimary" align="center">Github       Linkedin</Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <Box padding={5}>
+                        <Link href="https://linktr.ee/ColaboradoresProjetoIntegrador">
                         <img className='perfis' src="https://i.pinimg.com/originals/54/bf/7a/54bf7a45856c608fe7165a908d57c7cf.png" alt="Foto de um integrante" />
-                        <Typography  className='fontSans' color="textPrimary" align="center">Nome Completo</Typography>
-                        <Typography  className='fontSans' color="textPrimary" align="center">Github       Linkedin</Typography>
+                        </Link>
+                        <Typography  className='fontSans' color="textPrimary" align="center">Melissa Julia </Typography>
+                        <Typography  className='fontSans' color="textPrimary" align="center">São Paulo, SP</Typography>
                     </Box>
                 </Grid>
             </Grid>
