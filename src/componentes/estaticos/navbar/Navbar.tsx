6da4ec58b120a6c904f6b96f8d8088393072c11c
Vitor Martins <vitormartinsxd@outlook.com>
2022-09-15 10:20:24 -0300

    Adicionando a pagina quero ajudar, e renderizando a NavBar.

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -37,7 +37,7 @@ function Navbar() {
     navbarComponent = (
       <AppBar position="static">
         <Toolbar variant="dense">
-          <Box className="cursor">
+          <Box mx={2} className="cursor">
             <Typography variant="h5" className="colornav">
               <div className="logo">
                 <Link to="/sobrenos">
@@ -54,39 +54,26 @@ function Navbar() {
             </Typography>
           </Box>
 
-          <Box display="flex" justifyContent="start">
+          <Box className="font" display="flex" justifyContent="start">
             <Link to="/home" className="text-decorator-none">
-              <Box mx={1} className="cursor">
+              <Box mx={2} className="cursor">
                 <Typography variant="h6" className="colornav">
-                  Home
+                  HOME
                 </Typography>
               </Box>
             </Link>
-            <Link to="/posts" className="text-decorator-none">
-              <Box mx={1} className="cursor">
+            
+            <Link to="/ajudar" className="text-decorator-none">
+              <Box mx={2} className="cursor">
                 <Typography variant="h6" className="colornav">
-                  Postagens
-                </Typography>
-              </Box>
-            </Link>
-            <Link to="/temas" className="text-decorator-none">
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" className="colornav">
-                  Temas
-                </Typography>
-              </Box>
-            </Link>
-            <Link to="/formularioTema" className="text-decorator-none">
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" className="colornav">
-                  Cadastrar tema
+                  QUERO AJUDAR
                 </Typography>
               </Box>
             </Link>
 
-            <Box mx={1} className="cursorp" onClick={goLogout}>
+            <Box mx={2} className="cursorp" onClick={goLogout}>
               <Typography variant="h6" className="colornav">
-                Logout
+                LOGOUT
               </Typography>
             </Box>
           </Box>
---
+++ src/paginas/queroAjudar/QueroAjudar.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import "./QueroAjudar.css";
+import { Box } from "@mui/material";
+import { Button, Grid, TextField} from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { TokenState } from "../../store/tokens/tokensReducer";
+import { toast } from "react-toastify";
+
+function QueroAjudar() {
+
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        "& > *": {
+          margin: theme.spacing(1),
+          width: "25ch",
+        },
+      },
+    })
+  );
+  let navigate = useNavigate();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+
+  useEffect(() => {
+    if (token == "") {
+      toast.error('Você precisa estar logado', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+    });
+      navigate("/login");
+    }
+  }, [token]);
+
+  function enviar() {
+    toast.success('Formulario enviado com sucesso!', {
+      position: "bottom-left",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      theme: "colored",
+      progress: undefined,
+      });
+
+      navigate('/home')
+  }
+
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: "silver" }}
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <h2>Entre em Contato!</h2>
+
+            <TextField
+              
+              id="filled-basic"
+              label="Nome"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              required
+            />
+
+            <TextField
+              
+              id="filled-basic"
+              type="email"
+              label="E-mail"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              required
+            />
+
+            <TextField
+              
+              id="outlined-multiline-static"
+              name="assunto"
+              label="Assunto"
+              margin="normal"
+              multiline
+              rows={4}
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Button
+              className="btn02"
+              type="submit"
+              variant="contained"
+              color="primary"
+              
+            >
+              Enviar
+            </Button>
+          </Box>
+        </Grid>
+        <Grid className="imagem5" item xs={6}></Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default QueroAjudar;
